32fd02a8650a26e1c19663dc26e0996e60e8b593
sylee <soyeun.lee5930@gmail.com>
2023-10-12 18:24:31 +0900

    feat : subscribe 작업중

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main/Main';
+import Subscribe from './pages/Subscribe/Subscribe';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/subscribe" element={<Subscribe />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Subscribe/Subscribe.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Subscribe.scss';
+
+const Subscribe = () => {
+  return (
+    <div className="subscribe">
+      <section>
+        <div className="sectionInner">
+          <h1>Subscribe</h1>
+          <p>WEE.T를 구독하시고 트레이너의 맞춤 관리를 경험해보세요.</p>
+        </div>
+      </section>
+      <section className="subscribeContent">
+        <div className="sectionInner">
+          <div className="row">1</div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Subscribe;
