cb3b223ffcadd5d830b2697bb8ab566012df2aa2
kgy8987 <rlarldud8987@naver.com>
2023-10-17 14:47:43 +0900

    feat : Nav바 1차기능구현

--- src/Router.js
+++ src/Router.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main/Main';
+import Nav from './components/Nav/Nav';
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <Nav />
       <Routes>
         <Route path="/" element={<Main />} />
       </Routes>
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,8 +1,64 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import './Nav.scss';
 
 const Nav = () => {
-  return <div />;
+  const navigate = useNavigate();
+  const isLogin = !!localStorage.getItem('token');
+
+  const goToSubscribe = () => {
+    if (!isLogin) {
+      alert('로그인 후 이용 가능합니다.');
+
+      return;
+    }
+
+    navigate('/subscribe');
+  };
+
+  const goToCommunity = () => {
+    navigate('/');
+  };
+
+  const goToGuideLine = () => {
+    navigate('/');
+  };
+
+  const goToLocation = () => {
+    navigate('/');
+  };
+
+  const goToCondition = () => {
+    navigate('/');
+  };
+
+  const handleLogauto = () => {
+    localStorage.removeItem('token');
+    alert('로그아웃 완료');
+    window.location.reload();
+  };
+
+  return (
+    <nav className="nav">
+      <div className="logo">
+        <img src="images/Logo.png" alt="메인로고사진없음" />
+      </div>
+      <div className="logoTwo">
+        <span onclick={goToSubscribe}>구독</span>
+        <span onclick={goToCommunity}>커뮤니티</span>
+        <span onclick={goToGuideLine}>운동법</span>
+        <span onclick={goToLocation}>위치</span>
+        <span onclick={goToCondition}>상태페이지</span>
+        <img src="images/chatImage.png" alt="알림이미지" />
+        {/* <img src="images/chatImage.png" alt="챌린지이미지" /> */}
+        {isLogin ? (
+          <span onClick={handleLogauto}>닉네임/로그아웃</span>
+        ) : (
+          <Link to="/">로그인/회원가입</Link>
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
