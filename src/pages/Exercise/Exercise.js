0127735b8a0454c526856241e3f080166546b3a9
sseyeon2 <sseyoun2@gmail.com>
2023-10-18 15:06:07 +0900

    운동법 페이지

---
+++ src/pages/Exercise/Exercise.js
@@ -0,0 +1,11 @@
+import './Exercise.scss';
+
+const Exercise = () => {
+  return (
+    <div className="Exercise">
+      <section className="exerciseInner flexCenter"></section>
+    </div>
+  );
+};
+
+export default Exercise;
