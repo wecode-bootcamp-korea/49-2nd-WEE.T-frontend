adfb8216ed1319c30385eed42f82cf98479000a1
Leechengwon <leecwee8497@gmail.com>
2023-10-19 20:31:53 +0900

    style : 타이틀 제목변경 스타일수정

--- src/pages/Location/Location.js
+++ src/pages/Location/Location.js
@@ -8,7 +8,7 @@ const Location = () => {
       <div className="locationContainer">
         <div className="mainTitle">
           <img className="logo" src="./images/logo2.png" alt="로고" />
-          <h1 className="title">내주변 인바디측정은 어디서?</h1>
+          <h1 className="title">내주변 검색</h1>
         </div>
 
         <div className="mapContainer">
