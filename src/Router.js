6900fc93ed034c63a89b19a42948ffbcb2bae996
kgy8987 <rlarldud8987@naver.com>
2023-10-19 10:00:28 +0900

    style : UI배치 및 수정

--- src/Router.js
+++ src/Router.js
@@ -4,7 +4,7 @@ import Main from './pages/Main/Main';
 import Login from './pages/Login/Login';
 import LoginKakao from './pages/Login/LoginKakao';
 import LoginNaver from './pages/Login/LoginNaver';
-import Community from './pages/Community/Community';
+import PostAdd from './pages/PostAdd/PostAdd';
 
 const Router = () => {
   return (
@@ -14,7 +14,7 @@ const Router = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/login/kakao" element={<LoginKakao />} />
         <Route path="/login/naver" element={<LoginNaver />} />
-        <Route path="/community" element={<Community />} />
+        <Route path="/postadd" element={<PostAdd />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/PostAdd/PostAdd.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import './PostAdd.scss';
+
+const PostAdd = () => {
+  return (
+    <div className="mainContainer">
+      <div className="feedContainer">
+        <div className="innerSection">
+          <div className="titleSection">WEET YOU ?</div>
+          <form
+            className="topSection"
+            method="post"
+            enctype="multipart/form-data"
+          >
+            <div className="fileBox">
+              <label for="chooseFile">💪 Upload Photo! 🏋️‍♀️</label>
+              <input
+                type="file"
+                id="chooseFile"
+                multiple
+                name="chooseFile"
+                accept="image/*"
+              />
+            </div>
+            <section className="centerSection">사진미리보기구현</section>
+          </form>
+          <section className="buttomSection">
+            <div className="challengeCheck">
+              <input type="checkbox" />
+              <span>챌린지참여</span>
+            </div>
+            <div className="textSection">
+              <textarea placeholder="피드를 작성해주세요." />
+            </div>
+            <div className="buttonArea">
+              <button>취소</button>
+              <button>게시</button>
+            </div>
+          </section>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PostAdd;
