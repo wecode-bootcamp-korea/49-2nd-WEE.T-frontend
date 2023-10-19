581537ba85c2922ac8ec75e42d2a558eb9cbea6d
sseyeon2 <sseyoun2@gmail.com>
2023-10-19 18:47:06 +0900

    식단 페이지 ui완성 및 목데이터 연결 완

--- src/pages/Exercise/Exercise.js
+++ src/pages/Exercise/Exercise.js
@@ -1,8 +1,30 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSearchParams, useNavigate } from 'react-router-dom';
 import './Exercise.scss';
+import DietContainer from '../../components/DietContainer/DietContainer';
 
 const Exercise = () => {
+  const [exerciseData, setExerciseData] = useState();
+  const breakfastIcon = '/images/icon-sun.png';
+  const lunchIcon = '/images/icon-lunch-sun.png';
+  const dinnerIcon = '/images/icon-moon.png';
+
+  useEffect(() => {
+    fetch('/data/exercise.json', {
+      method: 'GET',
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        if (result.message === 'READ_SUCCESS') {
+          setExerciseData(result.data);
+        } else {
+          alert('오류입니다. 관리자에게 문의하세요.');
+        }
+      });
+  }, []);
+  if (!exerciseData) {
+    return null;
+  }
   return (
     <div className="Exercise">
       <section className="sectionInner">
@@ -18,60 +40,86 @@ const Exercise = () => {
             <p className="badge">
               <img src="/images/icon-bread.png" alt="빵 아이콘" />
             </p>
-            <span className="userNickname">김세연</span>
+            <span className="userNickname">{exerciseData.nickname}</span>
             님의 하루 섭취 권장 칼로리는
-            <span className="userCal"> 1600cal </span>
+            <span className="userCal">
+              {' '}
+              {exerciseData.dailyCalorieIntake}kcal{' '}
+            </span>
             입니다.
           </div>
           <div className="userNutrient">
             <p className="NutrientCircle">
               <span className="nutrientText">탄수화물</span>
               <span className="carbonhydrate nutrientNum">
-                52<span className="fontSizeThin">g</span>
+                {exerciseData.carbohydrate}
+                <span className="fontSizeThin">g</span>
               </span>
             </p>
             <p className="protein NutrientCircle">
               <span className="nutrientText">단백질</span>
               <span className="protein nutrientNum">
-                60<span className="fontSizeThin">g</span>
+                {exerciseData.protein}
+                <span className="fontSizeThin">g</span>
               </span>
             </p>
             <p className="fat NutrientCircle">
               <span className="nutrientText">지방</span>
               <span className="fat nutrientNum">
-                20<span className="fontSizeThin">g</span>
+                {exerciseData.fat}
+                <span className="fontSizeThin">g</span>
               </span>
             </p>
           </div>
         </div>
         <div className="dietContentWrapper">
-          <div className="todayDietContainer">
+          <div className="todayDietContainer dietContainer">
             <h1 className="todayText">
-              &nbsp; &nbsp; 김세연님 오늘의 식단&nbsp; &nbsp;
+              <p className="todayDate">2023.10.18</p>
+              &nbsp; &nbsp; {exerciseData.nickname}님 오늘의 식단&nbsp; &nbsp;
             </h1>
-            <div className="breakfastContainer">
-              <h2 className="breakfastText timeText">아침 추천 식단</h2>
-              <ul className="dietBoxWrapper">
-                <li className="dietBox">
-                  <p className="imgBox">
-                    <img src="/images/banana.jpg" alt="식단1"></img>
-                  </p>
-                  <div className="dietInfo">
-                    <p className="nutrientName infoText">탄수화물</p>
-                    <p className="foodName infoText">
-                      바나나<span className="gram">&nbsp;&nbsp;[56g]</span>
-                    </p>
-                    <p className="foodCount infoText">
-                      1&nbsp;<span className="numerator">1</span>&#47;
-                      <span className="denominator">4</span>개
-                    </p>
-                    <p className="foodInfo infoText">
-                      바나나는 식이섬유가 풍부하여 변비에 효과적인 식품입니다.
-                    </p>
-                  </div>
-                </li>
-              </ul>
-            </div>
+            <DietContainer
+              ExerciseData={exerciseData.today.breakfast}
+              mealTime="아침"
+              iconImg={breakfastIcon}
+            />
+            <DietContainer
+              ExerciseData={exerciseData.today.lunch}
+              mealTime="점심"
+              iconImg={lunchIcon}
+            />
+            <DietContainer
+              ExerciseData={exerciseData.today.dinner}
+              mealTime="저녁"
+              iconImg={dinnerIcon}
+            />
+          </div>
+          <div className="todayDietContainer dietContaine">
+            <h1 className="todayText">
+              <p className="todayDate">2023.10.19</p>
+              &nbsp; &nbsp; 김세연님 내일 식단&nbsp; &nbsp;
+            </h1>
+            {exerciseData && (
+              <DietContainer
+                ExerciseData={exerciseData.tomorrow.breakfast}
+                mealTime="아침"
+                iconImg={breakfastIcon}
+              />
+            )}
+            {exerciseData && (
+              <DietContainer
+                ExerciseData={exerciseData.tomorrow.lunch}
+                mealTime="점심"
+                iconImg={lunchIcon}
+              />
+            )}
+            {exerciseData && (
+              <DietContainer
+                ExerciseData={exerciseData.tomorrow.dinner}
+                mealTime="저녁"
+                iconImg={dinnerIcon}
+              />
+            )}
           </div>
         </div>
       </section>
