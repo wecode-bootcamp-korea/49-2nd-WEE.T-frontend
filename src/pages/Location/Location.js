1d1f9ae0367ed5aa02391af85075c3b0dd6e82d7
Leechengwon <leecwee8497@gmail.com>
2023-10-19 18:40:34 +0900

    feat : 카카오 지도 불러오기

--- src/pages/Location/Location.js
+++ src/pages/Location/Location.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './Location.scss';
+import KakaoMap from './KakaoMap/KakaoMap';
+
 const Location = () => {
   return (
     <div className="location">
       <div className="locationContainer">
         <div className="mainTitle">
           <img className="logo" src="./images/logo2.png" alt="로고" />
-          <h1 className="title">내주변 인바디는 측정은 어디서?</h1>
+          <h1 className="title">내주변 인바디측정은 어디서?</h1>
+        </div>
+
+        <div className="mapContainer">
+          <KakaoMap />
         </div>
       </div>
     </div>
