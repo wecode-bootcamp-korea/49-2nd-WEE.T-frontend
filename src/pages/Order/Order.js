35c88d020512d18c2636e83793537d5aab295830
sylee <soyeun.lee5930@gmail.com>
2023-10-23 22:03:18 +0900

    style : order page UI 구현 중

--- src/pages/Order/Order.js
+++ src/pages/Order/Order.js
@@ -20,19 +20,41 @@ const Order = () => {
           </div>
           <table className="subTable">
             <tr>
-              <td>Lorem</td>
-              <td>Ipsum</td>
-              <td>Dolor</td>
+              <td>구독 개월수</td>
+              <td>상품 금액</td>
+              <td>할인 금액</td>
+              <td>결제 금액</td>
             </tr>
             <tr>
-              <td>Lorem</td>
-              <td>Lorem Ipsum Dolor Lorem Ipsum Dolor</td>
-              <td>Dolor</td>
+              <td>1개월</td>
+              <td>9,800원</td>
+              <td>(-)4,900원</td>
+              <td>4,900원</td>
             </tr>
           </table>
         </div>
       </section>
-      <section>2</section>
+      <section className="paymentMethod sectionInner">
+        <div className="methodTitle">결제 방법</div>
+        <div className="selectMethod">
+          <div className="buttonDiv">
+            <button type="button">
+              <img src="/images/logo-toss-pay.png" alt="토스페이" />
+            </button>
+          </div>
+          <div className="buttonDiv">
+            <button type="button">
+              <img src="/images/payment_small.png" alt="카카오페이" />
+            </button>
+          </div>
+          <div className="buttonDiv">
+            <button type="button">신용·체크카드</button>
+          </div>
+          <div className="buttonDiv">
+            <button type="button">가상계좌</button>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
