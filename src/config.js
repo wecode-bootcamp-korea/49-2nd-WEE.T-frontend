fe0437999e8ad56a31ecfe0c5f87b01d0a7cc11a
sseyeon2 <sseyoun2@gmail.com>
2023-10-23 19:36:51 +0900

    nav수정

--- src/config.js
+++ src/config.js
@@ -4,3 +4,4 @@ export const LOGIN_NAVER_API = 'http://10.58.52.68:8000/auth/naver/login';
 export const LOGIN_KAKAO_API = 'http://10.58.52.68:8000/auth/kakao/login';
 export const DIET_API = 'http://10.58.52.68:8000/diets';
 export const LOGIN_AWS_API = 'http://3.39.73.27:8000';
+/*export const LOGIN_AWS_API = 'http://10.58.52.68:8000';*/
