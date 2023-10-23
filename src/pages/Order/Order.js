3c1514d855b0dfc103c874b0416a6f9ab134486b
sylee <soyeun.lee5930@gmail.com>
2023-10-23 19:42:56 +0900

    feat : order UI 구현중

--- src/pages/Order/Order.js
+++ src/pages/Order/Order.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import { useLocation } from 'react-router';
-// import './Order.scss';
+import './Order.scss';
 
 const Order = () => {
   const location = useLocation();
   console.log(location.state);
-  return <div>결제페이지 테스트</div>;
+
+  return (
+    <div className="order">
+      <section className="subscribeInfo sectionInner">
+        <div className="subscribeTitle">구독상품 정보</div>
+        <div className="subscribeDiv">
+          <div className="subscribeImgAndMonth">
+            <img
+              className="subImg"
+              src="/images/subscribeImg.png"
+              alt="구독이미지"
+            />
+          </div>
+          <table className="subTable">
+            <tr>
+              <td>Lorem</td>
+              <td>Ipsum</td>
+              <td>Dolor</td>
+            </tr>
+            <tr>
+              <td>Lorem</td>
+              <td>Lorem Ipsum Dolor Lorem Ipsum Dolor</td>
+              <td>Dolor</td>
+            </tr>
+          </table>
+        </div>
+      </section>
+      <section>2</section>
+    </div>
+  );
 };
 
 export default Order;
