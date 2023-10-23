a44aebc03c0045b1928db3fd546da9adce9803d5
kgy8987 <rlarldud8987@naver.com>
2023-10-24 04:07:12 +0900

    fix : 전체코드 수정중

--- src/pages/PostAdd/PostAdd.js
+++ src/pages/PostAdd/PostAdd.js
@@ -1,77 +1,54 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './PostAdd.scss';
 
 const PostAdd = ({ isEdit }) => {
   const navigate = useNavigate();
-  const [images, setImages] = useState([]);
-  const [previewImages, setPreviewImages] = useState([]);
+  const [image, setImage] = useState([]);
+  const [text, setText] = useState('');
   const [isChecked, setIsChecked] = useState(0);
-  const [postContent, setPostContent] = useState({
-    imageUrl: [],
-    content: '',
-    challenge: '',
-  });
+  const [previewImage, setPreviewImage] = useState([]);
 
   const handleImageChange = (e) => {
-    const selectedImages = e.target.files;
-    if (selectedImages.length === 0) {
-      alert('이미지를 선택하세요');
-      return;
-    }
+    const selectImageList = e.target.files; //이벤트 발생시 받은 파일리스트(object)
+    const newImages = [...image]; // image배열을 복제, newImages에 할당
+    const newPreviewImages = [...previewImage]; //image배열을 복제, newPreviewImages에 할당
+    const maxImages = 3; //최대이미지는 3장
 
-    const newImages = [...images];
-    const newPreviewImages = [...previewImages];
-    const maxImages = 3;
-
-    // formData로 해야 함.
-
-    for (let i = 0; i < selectedImages.length; i++) {
+    for (let i = 0; i < selectImageList.length; i++) {
       if (newImages.length < maxImages) {
-        newImages.push(selectedImages[i]);
-        const imageUrl = URL.createObjectURL(selectedImages[i]);
-        newPreviewImages.push(imageUrl);
+        // 만일 newImages의 배열의 길이가 maxImages보다 작다면.(배열에 추가이미지를 허용하는 조건)
+        newImages.push(selectImageList[i]);
+        const previewImageUrl = URL.createObjectURL(selectImageList[i]); //미리보기가 가능하게 변수화
+        newPreviewImages.push(previewImageUrl); // previewImage배열을 복사한 newPreviewImages에 추가
       } else {
-        alert(`최대 3장까지 이미지를 등록할 수 있습니다.`);
+        alert('최대 3장까지 이미지를 등록 할 수있습니다.');
         break;
       }
     }
+    setImage(newImages); //setImage 함수가 실행, 복사된 newImages 의 배열을 사용, 이미지 상태업데이트.
+    setPreviewImage(newPreviewImages); //setPreviewImage 함수가 실행, newPreviewImages 배열을 사용, previewImage 상태업데이트
 
-    setImages(newImages);
-    setPreviewImages(newPreviewImages);
-
-    console.log(newImages);
+    // console.log(newImages);
   };
 
-  console.log(previewImages);
+  // console.log(previewImage);
 
   const handleRemoveImage = (index) => {
-    const newImages = [...images];
-    const newPreviewImages = [...previewImages];
+    const newPreviewImages = [...previewImage]; //image배열을 복제, newPreviewImages에 할당
+    newPreviewImages.splice(index, 1); //newPreviewImages배열에서 index위치에 있는 하나만 제거.
 
-    newImages.splice(index, 1);
-    newPreviewImages.splice(index, 1);
-
-    setImages(newImages);
-    setPreviewImages(newPreviewImages);
+    setPreviewImage(newPreviewImages);
   };
 
   const handleChecked = () => {
-    setIsChecked(isChecked === 0 ? 1 : 0);
-    setPostContent({
-      ...postContent,
-      challenge: isChecked === 0 ? 1 : 0,
-    });
-    console.log(postContent);
+    setIsChecked(isChecked === 0 ? 1 : 0); // setIsChecked호출하여 isChecked 변수의 값을 0일때1로, 1일때 0으로 변경.
+    // console.log('challenge', isChecked);
   };
 
   const handleText = (e) => {
-    const { name, value } = e.target;
-    setPostContent({
-      ...postContent,
-      [name]: value,
-    });
-    console.log(postContent);
+    setText(e.target.value);
+    // console.log('content', text);
   };
 
   const handleCancel = () => {
@@ -79,6 +56,13 @@ const PostAdd = ({ isEdit }) => {
   };
 
   const handlePost = () => {
+    const formData = new FormData();
+    formData.append('imageUrl', image);
+    formData.append('content', text);
+    formData.append('challenge', isChecked);
+
+    // console.log('FormData:', formData);
+
     fetch('http://10.58.52.247:8000/feeds', {
       method: 'POST',
       headers: {
@@ -87,11 +71,11 @@ const PostAdd = ({ isEdit }) => {
         //Authorization: "accessToken",
         // 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NSwiaXNOZXciOmZhbHNlLCJpYXQiOjE2OTcwNzMxNzAsImV4cCI6MTY5NzExNjM3MH0.f-YMfUS7Qrlh4d69kXzZxqUEI4lCLanQAWqQeYcoI3U',
       },
-      body: JSON.stringify({
-        imageUrl: previewImages,
-        content: postContent.content,
-        challenge: postContent.challenge,
-      }),
+      body: formData,
+      // JSON.stringify({
+      //   imageUrl: previewImages,
+      //   content: postContent.content,
+      //   challenge: postContent.challenge,
     })
       .then((res) => res.json())
       .then((result) => {
@@ -102,13 +86,12 @@ const PostAdd = ({ isEdit }) => {
         }
       });
   };
-
   // useEffect(() => {
   //   if (isEdit) {
   //     alert('post data get!');
-  //     // fetch('수정하기 위한 post get')
-  //     //   .then((res) => res.json())
-  //     //   .then((result) => setPostContent(result));
+  //     fetch('수정하기 위한 post get')
+  //       .then((res) => res.json())
+  //       .then((result) => setPostContent(result));
   //   }
   // }, []);
 
@@ -119,7 +102,7 @@ const PostAdd = ({ isEdit }) => {
         <div className="innerSection">
           <div className="titleSection">WEET YOU ?</div>
           <div className="prvSection">
-            {previewImages.map((image, index) => (
+            {previewImage.map((image, index) => (
               <div key={index} className="previewList">
                 <img src={image} alt="preview 이미지없음" />
                 <button
@@ -142,7 +125,7 @@ const PostAdd = ({ isEdit }) => {
                 type="file"
                 id="chooseFile"
                 multiple
-                name="imageUrl"
+                name={image}
                 accept="image/*"
                 onChange={handleImageChange}
               />
@@ -150,7 +133,11 @@ const PostAdd = ({ isEdit }) => {
           </form>
           <section className="buttomSection">
             <div className="challengeCheck">
-              <input onChange={handleChecked} type="checkbox" />
+              <input
+                onChange={handleChecked}
+                type="checkbox"
+                name={isChecked}
+              />
               <span>챌린지참여</span>
             </div>
             <div className="textSection">
@@ -158,12 +145,14 @@ const PostAdd = ({ isEdit }) => {
                 onChange={handleText}
                 placeholder="피드를 작성해주세요."
                 maxLength={100}
-                name="content"
+                name={text}
               />
             </div>
             <div className="buttonArea">
               <button onClick={handleCancel}>취소</button>
-              <button onClick={handlePost}>{isEdit ? '수정' : '작성'}</button>
+              <button type="submit" onClick={handlePost}>
+                {isEdit ? '수정' : '작성'}
+              </button>
             </div>
           </section>
         </div>
