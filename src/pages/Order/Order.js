1bab43c62adfab2904feaf61344fe0d3a8aefa48
kgy8987 <rlarldud8987@naver.com>
2023-10-24 04:17:19 +0900

    feat : 결제페이지UI 구현중

--- src/pages/Order/Order.js
+++ src/pages/Order/Order.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import { useLocation } from 'react-router';
-// import './Order.scss';
+// import { useLocation } from 'react-router';
+import './Order.scss';
 
 const Order = () => {
-  const location = useLocation();
-  console.log(location.state);
   return <div>결제페이지 테스트</div>;
 };
 
