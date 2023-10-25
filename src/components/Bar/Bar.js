e106aa3bffd637f78e27d5996bdcd9498fc5589f
Leechengwon <leecwee8497@gmail.com>
2023-10-26 01:52:24 +0900

    '회원가입후 최초 목표값없을때 기능추가 체지방 골격근'

--- src/components/Bar/Bar.js
+++ src/components/Bar/Bar.js
@@ -1,21 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import './Bar.scss';
+import { useNavigate } from 'react-router-dom';
+import Popup from '../Popup/Popup';
 
 const offset = 0.1; // width 증감 offset
 const animateTime = 1; // animation 지속시간
 
 const Bar = (props) => {
   const { label, unit, total, value } = props;
-
   const [percentage, setPercentage] = useState(0);
-
   const [width, setWidth] = useState(0); //width is percentage value;
+  const [backgroundColor, setBackgroundColor] = useState('#5e3dea');
+  const [isPopup, setIsPopup] = useState(false);
+  const navigate = useNavigate();
 
-  const [backgroundColor, setBackgroungColor] = useState('#5e3dea');
+  const goEdit = () => {
+    navigate('/edit');
+    setIsPopup(false);
+  };
+
+  const goUpdate = () => {
+    setIsPopup(true);
+  };
 
   useEffect(() => {
     const _percentage = (value / total) * 100;
-    if (_percentage > 100) setBackgroungColor('red');
+    if (_percentage > 100) setBackgroundColor('red');
     setPercentage(_percentage > 100 ? 100 : _percentage);
   }, [total, value]);
 
@@ -42,7 +52,15 @@ const Bar = (props) => {
     <div className="bar">
       <div className="look">
         <span className="barLabel">{label}</span>
-        <span className="valueAndUnit">{`${value} ${unit}`}</span>
+
+        {value === undefined ? (
+          <span className="valueAndUnit">{`${value} ${unit}`} </span>
+        ) : (
+          <button
+            onClick={goUpdate}
+            className="valueUndefined"
+          >{`${value} ${unit}`}</button>
+        )}
       </div>
       <div className="barBox">
         <div
@@ -53,6 +71,17 @@ const Bar = (props) => {
           }}
         />
       </div>
+      {isPopup ? (
+        <Popup
+          title="내상태관리 가기"
+          leftBtnValue="이동"
+          rightBtnValue="닫기"
+          rightBtnClick={() => {
+            setIsPopup(false);
+          }}
+          leftBtnClick={goEdit}
+        />
+      ) : null}
     </div>
   );
 };
