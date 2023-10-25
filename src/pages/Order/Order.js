1d191d85e4c606ec36f465e6a4f8396a78dac3ee
kgy8987 <rlarldud8987@naver.com>
2023-10-25 12:04:43 +0900

    test

--- src/pages/Order/Order.js
+++ src/pages/Order/Order.js
@@ -1,28 +1,76 @@
 import React from 'react';
-// import { useLocation } from 'react-router';
+import { useLocation } from 'react-router';
 import './Order.scss';
 
 const Order = () => {
+  const location = useLocation();
+  const subscribeId = location.state.subscribeId;
+  console.log(subscribeId);
+
+  const fetchOrder = () => {
+    fetch(`EndPoint/subscription`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        // "Authorization": TOKEN
+      },
+      body: JSON.stringify({
+        subscribeId,
+      }),
+    }).then((res) => {
+      console.log(res);
+    });
+  };
+
   return (
-    <div className="orderMain">
-      <div className="orderMainContainer">
-        <div className="orderEntireSection">
-          <div className="topSection">주문자내역</div>
-          <div className="middleSection">중간섹션</div>
-          <div className="buttomSection">
-            <div className="PaymentType">
-              <p>결 제 방 식</p>
-              <div className="typeBtn">
-                <button>카 카 오 페 이</button>
-                <button>무 통 장 결 제</button>
-              </div>
-            </div>
-            <div className="finalBtn">
-              <button>결 제</button>
-            </div>
+    <div className="order">
+      <section className="subscribeInfo sectionInner">
+        <div className="subscribeTitle">구독상품 정보</div>
+        <div className="subscribeDiv">
+          <div className="subscribeImgAndMonth">
+            <img
+              className="subImg"
+              src="/images/subscribeImg.png"
+              alt="구독이미지"
+            />
+          </div>
+          <table className="subTable">
+            <tr>
+              <td>구독 개월수</td>
+              <td>상품 금액</td>
+              <td>할인 금액</td>
+              <td>결제 금액</td>
+            </tr>
+            <tr>
+              <td>1개월</td>
+              <td>9,800원</td>
+              <td>(-)4,900원</td>
+              <td>4,900원</td>
+            </tr>
+          </table>
+        </div>
+      </section>
+      <section className="paymentMethod sectionInner">
+        <div className="methodTitle">결제 방법</div>
+        <div className="selectMethod">
+          <div className="buttonDiv">
+            <button type="button">
+              <img src="/images/logo-toss-pay.png" alt="토스페이" />
+            </button>
+          </div>
+          <div className="buttonDiv">
+            <button type="button">
+              <img src="/images/payment_small.png" alt="카카오페이" />
+            </button>
+          </div>
+          <div className="buttonDiv">
+            <button type="button">신용·체크카드</button>
+          </div>
+          <div className="buttonDiv">
+            <button type="button">가상계좌</button>
           </div>
         </div>
-      </div>
+      </section>
     </div>
   );
 };
